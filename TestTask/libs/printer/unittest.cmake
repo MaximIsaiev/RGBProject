26de3ae3624da1a7b657bf9b565ebd2c65a85035
sta.litvinenko <stepangg@yandex.ru>
2022-04-12 12:22:01 +0300

    feat-[]: поддкржка gtest, не требующая системной библиотеки GTest

--- TestTask/libs/printer/unittest.cmake
+++ TestTask/libs/printer/unittest.cmake
@@ -8,7 +8,7 @@ if(UNITTEST AND EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/test)
 
 	add_executable(${PROJECT_NAME}_t ${SRC})
 
-	target_link_libraries(${PROJECT_NAME}_t ${PROJECT_NAME} gtest pthread)
+	target_link_libraries(${PROJECT_NAME}_t ${PROJECT_NAME} gtest)
 
 	set_target_properties(${PROJECT_NAME}_t PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${TEST_OUTPUT_PATH})
 	add_test(NAME ${PROJECT_NAME}_t COMMAND ${PROJECT_NAME}_t WORKING_DIRECTORY ${TEST_OUTPUT_PATH})
