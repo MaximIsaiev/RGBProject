39e6b88f489dcbd65ca9b1583825a0817161a8e6
RustemZS <118213917+RustemZS@users.noreply.github.com>
2023-04-01 21:44:57 +0300

    [*] Add magic number search
    Я сделал поиск magic numbers на чистом c++. Сейчас он закомментирован, так как я не могу соединить твой код и мой. Код предполагает, что каналы идут друг за другом без рандомных данных между ними. Единственное чего боится код - это считывание данных канала как magic number.

--- rgb/rgbsimplebackend.cpp
+++ rgb/rgbsimplebackend.cpp
@@ -48,6 +48,109 @@ void RGBSimpleBackend::openByteArrayFile(QUrl filePath)
 
     // Где-то здесь будет происходить конвертация одномерного массива в двухмерный массив
     // и вообще вся магия
+/*//File size
+    fileRGB.seekg(0, std::ios::end);
+    int size = fileRGB.tellg();
+    std::cout << size << std::endl;
+    fileRGB.seekg(0, std::ios::beg);
+
+    std::vector<std::vector<uint8_t>> colorChannelsRGB(3);
+    uint32_t magicNumberRed = 0;
+    uint32_t magicNumberGreen = 0;
+    uint32_t magicNumberBlue = 0;
+    uint32_t magicNumberRead = 0;
+    int startRedChannel = 4;
+    int endRedChannel = 0;
+    int startGreenChannel = 0;
+    int endGreenChannel = 0;
+    int startBlueChannel = 0;
+    int endBlueChannel = 0;
+    int position = startRedChannel;
+    std::vector<uint8_t> redData;
+    std::vector<uint8_t> greenData;
+    std::vector<uint8_t> blueData;
+
+    //Red channel
+    fileRGB.read((char*)&magicNumberRed, sizeof magicNumberRed); printf("%#x\n", magicNumberRed);
+    while (position <= size - 4)
+    {
+        fileRGB.seekg(position, std::ios_base::beg);
+        fileRGB.read((char*)&magicNumberRead, sizeof magicNumberRead);
+        if (magicNumberRead == magicNumberRed)
+        {
+            endRedChannel = position;
+            break;
+        }
+        position++;
+    }
+    int lengthOfColorData = endRedChannel - startRedChannel;
+
+    uint8_t redPixel = 0;
+    for (int i = startRedChannel; i < endRedChannel; i++)
+    {
+        fileRGB.seekg(i, std::ios::beg);
+        fileRGB.read((char*)&redPixel, sizeof(redPixel));
+        redData.push_back(redPixel);
+        //printf("%d\n", redPixel);
+    }
+    colorChannelsRGB[0].resize(lengthOfColorData);
+    colorChannelsRGB[0].assign(redData.begin(), redData.end());
+
+    //GreenChanel
+    fileRGB.seekg(endRedChannel + 4, std::ios_base::beg);
+    startGreenChannel = endRedChannel + 4;
+    position = startGreenChannel + 4;
+    fileRGB.read((char*)&magicNumberGreen, sizeof magicNumberGreen); printf("%#x\n", magicNumberGreen);
+    while (position <= size - 4)
+    {
+        fileRGB.seekg(position, std::ios_base::beg);
+        fileRGB.read((char*)&magicNumberRead, sizeof magicNumberRead);
+        if (magicNumberRead == magicNumberGreen)
+        {
+            endGreenChannel = position;
+            break;
+        }
+        position++;
+    }
+
+    uint8_t greenPixel = 0;
+    for (int i = startGreenChannel; i < endGreenChannel; i++)
+    {
+        fileRGB.seekg(i, std::ios::beg);
+        fileRGB.read((char*)&greenPixel, sizeof(greenPixel));
+        greenData.push_back(greenPixel);
+        //printf("%d\n", greenPixel);
+    }
+    colorChannelsRGB[1].resize(lengthOfColorData);
+    colorChannelsRGB[1].assign(greenData.begin(), greenData.end());
+
+    //BlueChanel
+    fileRGB.seekg(endGreenChannel + 4, std::ios_base::beg);
+    startBlueChannel = endGreenChannel + 4;
+    position = startBlueChannel + 4;
+    fileRGB.read((char*)&magicNumberBlue, sizeof magicNumberBlue); printf("%#x\n", magicNumberBlue);
+    while (position <= size - 4)
+    {
+        fileRGB.seekg(position, std::ios_base::beg);
+        fileRGB.read((char*)&magicNumberRead, sizeof magicNumberRead);
+        if (magicNumberRead == magicNumberBlue)
+        {
+            endBlueChannel = position;
+            break;
+        }
+        position++;
+    }
+
+    uint8_t bluePixel = 0;
+    for (int i = startBlueChannel; i < endBlueChannel; i++)
+    {
+        fileRGB.seekg(i, std::ios::beg);
+        fileRGB.read((char*)&bluePixel, sizeof(bluePixel));
+        blueData.push_back(bluePixel);
+        //printf("%d\n", bluePixel);
+    }
+    colorChannelsRGB[2].resize(lengthOfColorData);
+    colorChannelsRGB[2].assign(blueData.begin(), blueData.end());*/
 
     QImage image;
     image.loadFromData(byteArray);
